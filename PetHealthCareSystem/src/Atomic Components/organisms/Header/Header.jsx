b680d9786733a9c5a2e64dc151dfdaf8268e8d96
Stayer1231 <phongntse171371@fpt.edu.vn>
2024-06-03 23:15:50 +0700

    add: header

--- PetHealthCareSystem/src/Atomic Components/organisms/Header/Header.jsx
+++ PetHealthCareSystem/src/Atomic Components/organisms/Header/Header.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import './Header.scss'
+import Logo from "../../../assets/img/dog_logo.jpg"
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <header className='header-container'>
-            <div className='logo-container'></div>
+            <div className='logo-container'>
+                <img src={Logo} alt="" />
+            </div>
             <nav className='nav-container'>
                 <ul className='nav-list'>
-                    <li className='nav-item'>Home</li>
-                    <li className='nav-item'>About</li>
-                    <li className='nav-item'>Services</li>
-                    <li className='nav-item'>Contact</li>
+                    <Link to='/' className='nav-item'>
+                        <li className='nav-item'>Home</li>
+                    </Link>
+                    <Link to='/about' className='nav-item'>
+                        <li className='nav-item'>About</li>
+                    </Link>
+                    <Link to='/services' className='nav-item'>
+                        <li className='nav-item'>Services</li>
+                    </Link>
+                    <Link to='/contact' className='nav-item'>
+                        <li className='nav-item'>Contact</li>
+                    </Link>
                 </ul>
             </nav>
         </header>
