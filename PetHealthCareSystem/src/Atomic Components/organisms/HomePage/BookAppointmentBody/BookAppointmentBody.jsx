a0268f0af1e59c664854998e242d20fb76a304f7
SE171507_Duy Anh <anhbdse171507@fpt.edu.vn>
2024-06-07 16:16:59 +0700

    Add: BookAppointmentBody, HomePageBackground / Update: HomePageTemplate

---
+++ PetHealthCareSystem/src/Atomic Components/organisms/HomePage/BookAppointmentBody/BookAppointmentBody.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Text from "../../../atoms/Text/Text";
+import Button from "../../../atoms/Button/Button";
+import './BookAppointmentBody.scss'
+
+function BookAppointmentBody(){
+    return (
+        <>
+        <div className='user-book-appointment-controller-container'>
+                <div className="user-book-appointment-controller">
+                    <Text
+                        content={'Everything Veterinary Care For Your Everything Companion'}
+                        className={'user-book-appointment-controller-title-text'} 
+                        type={'h2'}
+                        />
+
+                    <Text
+                        content={'Primary, specialty, & emergency care that meets all of your pets healthcare needs.'}
+                        className={"user-book-appointment-controller-detail-text"} 
+                        type={'h4'}
+                        />
+
+                    <Button
+                        content="Book Appointment"
+                        className={'user-book-appointment-controller-button'}
+                    />
+                </div>
+                <div className="user-book-appointment-controller-img-container">
+                    <img src="https://images.ctfassets.net/8hq8guzcncfs/gBh0U7Yt94KdxO8H6bvzV/f76e3d19224091f28fda339a6b640396/d2.png?fm=webp" alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default BookAppointmentBody;
---
+++ PetHealthCareSystem/src/Atomic Components/organisms/HomePage/HomePageBackground/HomePageBackGround.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './HomePageBackGround.scss';
+
+function HomePageBackground(){
+    return(
+        <div className="background-image-container">
+            <img src="https://i0.wp.com/www.knoxalliance.store/wp-content/uploads/2017/05/light-color-background-images-for-website-top-hd-images-for-free-light-color-background-images-for-website-1-1024x768.jpg?ssl=1" 
+            alt=""
+            className="background-image"
+            />
+        </div>
+    )
+}
+
+export default HomePageBackground;
