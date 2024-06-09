b9e9e2f882d497106417a89708451fd349ae98fe
SE171507_Duy Anh <anhbdse171507@fpt.edu.vn>
2024-06-09 21:47:15 +0700

    Add: HomePageServiceBlock, ServiceCard / Update: BookAppointmentBody

---
+++ PetHealthCareSystem/src/Atomic Components/organisms/HomePage/HomePageServiceBlock/HomePageServiceBlock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './HomePageServiceBlock.scss';
+
+function HomePageServiceBlock(){
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default HomePageServiceBlock;
