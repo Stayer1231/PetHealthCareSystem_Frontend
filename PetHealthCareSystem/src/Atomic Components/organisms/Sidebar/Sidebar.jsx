0bbd5f693095d6a945382f165a864c274aa3d7b4
Stayer1231 <phongntse171371@fpt.edu.vn>
2024-06-13 11:20:50 +0700

    update: submit function for create pet, sidebar, css

--- PetHealthCareSystem/src/Atomic Components/organisms/Sidebar/Sidebar.jsx
+++ PetHealthCareSystem/src/Atomic Components/organisms/Sidebar/Sidebar.jsx
@@ -18,7 +18,7 @@ function Sidebar() {
 		species: "",
 		breed: "",
 		dateOfBirth: "",
-		isNeutered: false,
+		isNeutered: null,
 	});
 
 	// OPENING ADD PET MODAL
@@ -31,6 +31,12 @@ function Sidebar() {
 	const PetProfileMenuClicked = () => {
 		setSubmenuActive(!submenuActive);
 	};
+
+	// HANDLE SUBMIT ADD PET FORM
+	const handleAddPet = (e) => {
+		e.preventDefault();
+		console.log(petData);
+	}
 
 	// === PET MODAL ===
 	const openAddPetModal = () => {
@@ -146,112 +152,125 @@ function Sidebar() {
 								type={"h3"}
 							/>
 						</div>
-						<div className="adding-information-container">
-							{/* PET NAME */}
-							<div className="pet-name input-div">
-								<Text
-									content={"What is your pet's name?"}
-									className={"field-label required-field"}
-								/>
-								<input
-									type="text"
-									className="general-input-field"
-									value={petData.name}
-									onChange={(e) => setPetData(prev => ({ ...prev, name: e.target.value }))}
-								/>
+						<form onSubmit={handleAddPet}>
+							<div className="adding-information-container">
+								{/* PET NAME */}
+								<div className="pet-name input-div">
+									<Text
+										content={"What is your pet's name?"}
+										className={"field-label required-field"}
+									/>
+									<input
+										type="text"
+										className="general-input-field"
+										value={petData.name}
+										placeholder="Enter your pet's name"
+										onChange={(e) => setPetData(prev => ({ ...prev, name: e.target.value }))}
+									/>
+								</div>
+
+								{/* PET SPECIES */}
+								<div className="pet-role input-div">
+									<Text
+										content={"Assign my pet as?"}
+										className={"field-label required-field"}
+									/>
+									<select
+										className="general-input-field"
+										value={petData.species}
+										onChange={(e) => setPetData(prev => ({ ...prev, species: e.target.value }))}
+									>
+										<option value="" disabled>Select role of your pet</option>
+										<option value="dog">Dog</option>
+										<option value="cat">Cat</option>
+									</select>
+								</div>
+
+								{/* PET BREED */}
+								<div className="pet-breed input-div">
+									<Text
+										content={"What breed is your pet?"}
+										className={"field-label required-field"}
+									/>
+									<input
+										type="text"
+										className="general-input-field"
+										value={petData.breed}
+										placeholder="Enter your pet's breed"
+										onChange={(e) => setPetData(prev => ({ ...prev, breed: e.target.value }))}
+									/>
+								</div>
+
+								{/* PET DOB */}
+								<div className="pet-dob input-div">
+									<Text
+										content={"What is your pet's date of birth?"}
+										className={"field-label required-field"}
+									/>
+									<input
+										type="date"
+										className="general-input-field"
+										value={petData.dateOfBirth}
+										onChange={(e) => setPetData(prev => ({ ...prev, dateOfBirth: e.target.value }))}
+									/>
+								</div>
+
+								{/* PET GENDER */}
+								<div className="pet-gender input-div">
+									<Text
+										content={"What is your pet's date of birth?"}
+										className={"field-label required-field"}
+									/>
+									<select
+										className="general-input-field"
+									>
+										<option value="" disabled>What is your pet gender</option>
+										<option value="male">Male</option>
+										<option value="female">Female</option>
+									</select>
+								</div>
+
+								{/* PET NEUTERED */}
+								<div className="pet-neutered input-div">
+									<Text
+										content={"Has your pet been neutered?"}
+										className={"field-label required-field"}
+									/>
+									<select
+										name=""
+										id=""
+										className="general-input-field"
+										value={petData.isNeutered === null ? "" : petData.isNeutered}
+										onChange={(e) => setPetData(prev => ({ ...prev, isNeutered: JSON.parse(e.target.value) }))}
+									>
+										<option value="" disabled>Is your pet neutered</option>
+										<option value={true}>Yes</option>
+										<option value={false}>No</option>
+									</select>
+								</div>
 							</div>
 
-							{/* PET ROLE */}
-							<div className="pet-role input-div">
-								<Text
-									content={"Assign my pet as?"}
-									className={"field-label required-field"}
-									onChange={(e) => setPetData(prev => ({ ...prev, species: e.target.value }))}
+							{/* BTN CONTAINER */}
+							<div className="action-btn-container">
+								<Button
+									content="Save"
+									variant="filled"
+									className={"btn"}
+									onClick={closeAddPetModal}
+									type={"submit"}
 								/>
-								<input
-									type="text"
-									className="general-input-field"
-								/>
-							</div>
-
-							{/* PET BREED */}
-							<div className="pet-breed input-div">
-								<Text
-									content={"What breed is your pet?"}
-									className={"field-label required-field"}
-								/>
-								<input
-									type="text"
-									className="general-input-field"
-									onChange={(e) => setPetData(prev => ({ ...prev, breed: e.target.value }))}
+								<Button
+									content="Cancel"
+									variant="transparent"
+									className={"btn"}
+									onClick={closeAddPetModal}
 								/>
 							</div>
-
-							{/* PET DOB */}
-							<div className="pet-dob input-div">
-								<Text
-									content={"What is your pet's date of birth?"}
-									className={"field-label required-field"}
-								/>
-								<input
-									type="date"
-									className="general-input-field"
-									onChange={(e) => setPetData(prev => ({ ...prev, dateOfBirth: e.target.value }))}
-								/>
-							</div>
-
-							{/* PET GENDER */}
-							<div className="pet-gender input-div">
-								<Text
-									content={"What is your pet's date of birth?"}
-									className={"field-label required-field"}
-								/>
-								<select
-									name=""
-									id=""
-									className="general-input-field"
-								>
-									<option value="male">Male</option>
-									<option value="female">Female</option>
-								</select>
-							</div>
-
-							{/* PET NEUTERED */}
-							<div className="pet-neutered input-div">
-								<Text
-									content={"Has your pet been neutered?"}
-									className={"field-label required-field"}
-								/>
-								<select
-									name=""
-									id=""
-									className="general-input-field"
-								>
-									<option value="yes">Yes</option>
-									<option value="no">No</option>
-								</select>
-							</div>
-						</div>
-
-						{/* BTN CONTAINER */}
-						<div className="action-btn-container">
-							<Button
-								content="Save"
-								variant="filled"
-								className={"btn"}
-								onClick={closeAddPetModal}
-							/>
-							<Button
-								content="Cancel"
-								variant="transparent"
-								className={"btn"}
-								onClick={closeAddPetModal}
-							/>
-						</div>
+						</form>
 					</div>
-				</ModalBody>
-			</Modal>
-		</div>
+				</ModalBody >
+			</Modal >
+		</div >
 	);
 }
 
--- PetHealthCareSystem/src/Atomic Components/templates/PetManagementTemplate/PetManagementTemplate.jsx
+++ PetHealthCareSystem/src/Atomic Components/templates/PetManagementTemplate/PetManagementTemplate.jsx
@@ -7,9 +7,9 @@ function PetManagementTemplate() {
 	return (
 		<>
 			<div className="pet-management-container">
-				<dir>
+				<div>
 					<Sidebar />
-				</dir>
+				</div>
 				<div className="main-information-container">
 					<Outlet />
 				</div>
