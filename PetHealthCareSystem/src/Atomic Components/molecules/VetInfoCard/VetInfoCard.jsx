ef6c31af13f6ac0afe9574034fcb8e847bd3b854
DAB1602 <anhbdse171507@fpt.edu.vn>
2024-06-20 15:19:34 +0700

    Add: SimplePetCard, TimeFrameCard Update: add date-fns package, ServiceCard, VetInfoCard, BookingForm, BookingPageTemplate, HomePageTemplate, store/bookingForm.js

--- PetHealthCareSystem/src/Atomic Components/molecules/VetInfoCard/VetInfoCard.jsx
+++ PetHealthCareSystem/src/Atomic Components/molecules/VetInfoCard/VetInfoCard.jsx
@@ -2,30 +2,34 @@ import React from "react";
 import "./VetInfoCard.scss";
 import Text from "../../atoms/Text/Text";
 
-function VetInfoCard({ data, displayMessage }) {
+function VetInfoCard({ data, displayMessage, onClick, isSelected }) {
 
     return (
         <>
-            <div className='vet-card-container'>
-                <img
-                    className="vet-img"
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwdIVSqaMsmZyDbr9mDPk06Nss404fosHjLg&s" alt="" />
-                <div className="vet-card-name">
-                    <Text className={'vet-name-text'} type={'p'} content={data.fullName} />
+            <div className={`vet-card-container ${isSelected ? 'selected' : ''}`} onClick={onClick}>
+                <div className="vet-card-image-container">
+                    <img
+                        src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwdIVSqaMsmZyDbr9mDPk06Nss404fosHjLg&s"}
+                        alt={data.fullName}
+                        className="vet-card-image"
+                    />
                 </div>
-                <div className="vet-card-certification">
-                    <Text className={'vet-certification-text'} type={'p'} content={data.email} />
+                <div className="vet-card-details-container">
+                    <div className="vet-card-name">
+                        <Text
+                            content={data.fullName}
+                            type={"h4"}
+                            className={"vet-card-name-content"}
+                        />
+                    </div>
+                    <div className="vet-card-sub-details">
+                        <Text
+                            content={data.email}
+                            type={"subtitle"}
+                            className={"vet-card-address-content"}
+                        />
+                    </div>
                 </div>
-                {!displayMessage ?
-                    <>
-                    </>
-                    :
-                    <>
-                        <div className="vet-card-message">
-                            <Text className={'vet-message-text'} type={'p'} content={data.address} />
-                        </div>
-                    </>
-                }
             </div>
         </>
     )
