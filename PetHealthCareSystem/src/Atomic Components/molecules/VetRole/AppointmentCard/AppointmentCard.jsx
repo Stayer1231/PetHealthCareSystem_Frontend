2ce347d7ff5004632a1844efe06cb70c7a119bac
Stayer1231 <phongntse171371@fpt.edu.vn>
2024-06-30 01:12:15 +0700

    update: view pet for vet api

--- PetHealthCareSystem/src/Atomic Components/molecules/VetRole/AppointmentCard/AppointmentCard.jsx
+++ PetHealthCareSystem/src/Atomic Components/molecules/VetRole/AppointmentCard/AppointmentCard.jsx
@@ -4,19 +4,32 @@ import Text from "../../../atoms/Text/Text";
 import CustomTooltip from "./../../Tooltip/Tooltip";
 import { Modal, ModalBody, ModalHeader } from "../../Modal/Modal";
 import Button from "../../../atoms/Button/Button";
+import { formatDate } from "../../../../config/convertDate";
+import PatientCard from "../PatientCard/PatientCard";
 
-function AppointmentCard({ name, date }) {
-	const [showModal, setShowModal] = useState(false);
+function AppointmentCard({ name, date, petList }) {
+	const [showMedicalRecord, setShowMedicalRecord] = useState(false);
+	const [showPetList, setShowPetList] = useState(false);
 
-	// HANLDE APPOINTMENT CARD CLICK
-	const handleShowModal = () => {
-		setShowModal(true);
+	// HANDLE MEDICAL RECORD MODAL
+	const handleShowMedicalRecord = () => {
+		setShowMedicalRecord(true);
 	};
 
-	// HANDLE MODAL CLOSE
-	const handleCloseModal = () => {
-		setShowModal(false);
+	const handleCloseMedicalRecord = () => {
+		setShowMedicalRecord(false);
 	};
+	// -------------------------------
+
+	// HANDLE PET LIST MODAL
+	const handleShowPetList = () => {
+		setShowPetList(true);
+	};
+
+	const handleClosePetList = () => {
+		setShowPetList(false);
+	};
+	// -------------------------------
 
 	// HANDLE FORM SUBMIT
 	const handleFormSubmit = (e) => {
@@ -27,18 +40,18 @@ function AppointmentCard({ name, date }) {
 		<div className="">
 			<div
 				className="appointment-card-container"
-				onClick={handleShowModal}
+				onClick={handleShowPetList}
 			>
 				{/* APPOINTMENT INFO */}
 				<div className="appointment-info">
 					<Text
-						content={"Name here"}
+						content={"Lịch khám"}
 						type={"primary"}
 						className={"content"}
 						cursor={"pointer"}
 					/>
 					<Text
-						content={"Date here"}
+						content={formatDate(date)}
 						type={"primary"}
 						className={"content"}
 						cursor={"pointer"}
@@ -54,9 +67,10 @@ function AppointmentCard({ name, date }) {
 				</div>
 			</div>
 
+			{/* MEDICAL RECORD MODAL */}
 			<Modal
-				onHide={handleCloseModal}
-				show={showModal}
+				onHide={handleCloseMedicalRecord}
+				show={showMedicalRecord}
 				size={"sm"}
 			>
 				<ModalHeader content={"Hồ Sơ Bệnh Của Thú Cưng"} />
@@ -125,6 +139,22 @@ function AppointmentCard({ name, date }) {
 							/>
 						</div>
 					</form>
+				</ModalBody>
+			</Modal>
+
+			{/* PET LIST MODAL */}
+			<Modal
+				onHide={handleClosePetList}
+				show={showPetList}
+				size={"sm"}
+			>
+				<ModalHeader content={"Danh sách thú cưng trong ngày"} />
+				<ModalBody>
+					<div className="">
+						{petList.length > 0
+							? petList.map((pet, index) => <PatientCard data={pet} />)
+							: null}
+					</div>
 				</ModalBody>
 			</Modal>
 		</div>
--- PetHealthCareSystem/src/Atomic Components/organisms/VetRole/PatientMangement/PatientMangementLayout.jsx
+++ PetHealthCareSystem/src/Atomic Components/organisms/VetRole/PatientMangement/PatientMangementLayout.jsx
@@ -1,77 +1,122 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PatientMangementLayout.scss";
 import CatImg from "../../../../assets/img/Cat.jpg";
 import PatientMedicalReport from "../../../molecules/VetRole/PatientMedicalReport/PatientMedicalReport";
 import Text from "../../../atoms/Text/Text";
 import Button from "../../../atoms/Button/Button";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { Backdrop, CircularProgress } from "@mui/material";
+import Cookies from "js-cookie";
+import APIInUse from "../../../../config/axios/AxiosInUse";
 
 function PatientMangementLayout() {
 	const navigate = useNavigate();
+	const id = Cookies.get("userId");
+	const { patientId } = useParams();
+	const [isLoading, setIsLoading] = useState(false);
+	const [patientInformation, setPatientInformation] = useState({});
 
+	// HANDLE VIEW MEDICAL RECORD
 	const handleViewMedicalRecord = () => {
-		navigate("/medical-record/patient-medical-record/1");
-	}
+		navigate(`/medical-record/patient-medical-record/${patientId}`);
+	};
+
+	// GET PATIENT INFORMATION
+	// useEffect(() => {
+	// 	const getPatientInformation = async () => {
+	// 		s;
+	// 		try {
+	// 			setIsLoading(true);
+	// 			const response = await APIInUse.get(`Patient/${patientId}`);
+
+	// 			// setPatientInformation(response?.data?.data);
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 		} finally {
+	// 			setIsLoading(false);
+	// 		}
+	// 	};
+
+	// 	getPatientInformation();
+	// }, []);
 
 	return (
-		<div className="patient-management-container">
-			<div className="btn-container text-right">
-				<Button content="Xem hồ sơ bệnh" variant="filled" onClick={handleViewMedicalRecord}/>
-			</div>
-			<div className="pet-overview-container">
-				<div className="img-container">
-					<img
-						src={CatImg}
-						alt="Pet Image"
+		<>
+			{isLoading && (
+				<Backdrop
+					sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+					open={isLoading}
+				>
+					<div className="flex flex-col justify-center items-center gap-2">
+						<CircularProgress color="inherit" />
+						<h1>Waiting</h1>
+					</div>
+				</Backdrop>
+			)}
+
+			<div className="patient-management-container">
+				<div className="btn-container text-right">
+					<Button
+						content="Xem hồ sơ bệnh"
+						variant="filled"
+						onClick={handleViewMedicalRecord}
 					/>
 				</div>
-				<div className="pet-information-container">
-					{/* PET NAME */}
-					<div className="information-div">
-						<Text
-							content={"Tên: "}
-							type={"h4"}
-							className={"text-label"}
-						/>
-						<Text
-							content={"Kakashi"}
-							type={"h6"}
-							className={"text-content"}
+				<div className="pet-overview-container">
+					<div className="img-container">
+						<img
+							src={CatImg}
+							alt="Pet Image"
 						/>
 					</div>
+					<div className="pet-information-container">
+						{/* PET NAME */}
+						<div className="information-div">
+							<Text
+								content={"Tên: "}
+								type={"h4"}
+								className={"text-label"}
+							/>
+							<Text
+								content={"Kakashi"}
+								type={"h6"}
+								className={"text-content"}
+							/>
+						</div>
 
-					{/* PET SPECIES */}
-					<div className="information-div">
-						<Text
-							content={"Loài: "}
-							type={"h4"}
-							className={"text-label"}
-						/>
-						<Text
-							content={"Mèo"}
-							type={"h6"}
-							className={"text-content"}
-						/>
-					</div>
+						{/* PET SPECIES */}
+						<div className="information-div">
+							<Text
+								content={"Loài: "}
+								type={"h4"}
+								className={"text-label"}
+							/>
+							<Text
+								content={"Mèo"}
+								type={"h6"}
+								className={"text-content"}
+							/>
+						</div>
 
-					{/* PET BREED */}
-					<div className="information-div">
-						<Text
-							content={"Giống: "}
-							type={"h4"}
-							className={"text-label"}
-						/>
-						<Text
-							content={"Sphynx"}
-							type={"h6"}
-							className={"text-content"}
-						/>
+						{/* PET BREED */}
+						<div className="information-div">
+							<Text
+								content={"Giống: "}
+								type={"h4"}
+								className={"text-label"}
+							/>
+							<Text
+								content={"Sphynx"}
+								type={"h6"}
+								className={"text-content"}
+							/>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<PatientMedicalReport />
-		</div>
+				<PatientMedicalReport />
+			</div>
+		</>
 	);
 }
 
